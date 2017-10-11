cfd40994ba16c857538f4984c2cf008ea1fb659d
Neil Nie <yongyang.nie@gmail.com>
2017-10-11 09:12:10 -0400

    Pretty much finished

--- membrane/membrane/GameViewController.swift
+++ membrane/membrane/GameViewController.swift
@@ -15,23 +15,52 @@ class GameViewController: NSViewController {
     
     // MARK: Actions
     
-    
     @IBAction func nadhTohad(_ sender: Any) {
         
         let protein = sceneView.scene?.rootNode.childNode(withName: "protein", recursively: true)!
+        let atpProtein = sceneView.scene?.rootNode.childNode(withName: "atp-protein", recursively: true)!
+        
         for i in 0..<1 {
             let nadh = sceneView.scene?.rootNode.childNode(withName: "nadh-\(i)", recursively: true)!
             nadh?.removeAllActions()
-            nadh?.runAction(SCNAction.move(by: subtract(vector1: (protein?.position)!, vector2: (nadh?.position)!), duration: 3), completionHandler: {
+            nadh?.runAction(SCNAction.move(by: subtract(vector1: (protein?.position)!, vector2: (nadh?.position)!), duration: 2), completionHandler: {
                 nadh?.removeFromParentNode()
                 
-                let hGeo = SCNSphere.init(radius: 2)
-                let h = SCNNode.init(geometry: hGeo)
-                h.position = (protein?.position)!
-                h.geometry?.firstMaterial?.diffuse.contents = NSColor.blue
-                h.runAction(SCNAction.move(by: self.subtract(vector1: (h.position),
-                                                             vector2: SCNVector3Make(h.position.x, h.position.y - 60, h.position.z)),
-                                           duration: 1))
+                //create a new H molecule
+                let h = self.createH(position: (protein?.position)!, name: "hydrogen-\(i)")
+                
+                //sequence of movements
+                let action1 = SCNAction.move(by: self.subtract(vector1: (h.position), vector2: SCNVector3Make(h.position.x, h.position.y - 60, h.position.z)), duration: 1.2)
+                let action2 = SCNAction.move(by: self.subtract(vector1:SCNVector3Make((atpProtein?.position.x)!, (atpProtein?.position.y)! - 30, (atpProtein?.position.z)!) , vector2: (h.position)), duration: 1.3)
+                let action3 = SCNAction.move(by: self.subtract(vector1: (atpProtein?.position)!, vector2: SCNVector3Make((atpProtein?.position.x)!, (atpProtein?.position.y)! + 110, (atpProtein?.position.z)!)), duration: 1)
+                h.runAction(SCNAction.sequence([action1, action2, action3]), completionHandler: {
+                    
+                    // add the yellow sphere representing ATP
+                    let adpGeo = SCNSphere.init(radius: 5)
+                    let adp = SCNNode.init(geometry: adpGeo)
+                    adp.geometry?.firstMaterial?.diffuse.contents = NSColor.yellow
+                    adp.position = SCNVector3Make(-3.0, -3.0, -3.0)
+                    h.addChildNode(adp)
+                    let finalLocation = SCNAction.move(by: self.subtract(vector1:SCNVector3Make(CGFloat(self.randomIntFrom(start: -10, to: 80)),
+                                                                                                CGFloat(self.randomIntFrom(start: -90, to: -130)),
+                                                                                                CGFloat(self.randomIntFrom(start: 180, to: 250))), vector2: h.position), duration: 1.5)
+                    let randomMovement = SCNAction.repeatForever(self.molecularAnimation())
+                    h.runAction(SCNAction.sequence([finalLocation, randomMovement]))
+                    
+                    //NAD
+                    let nadGeo = SCNSphere.init(radius: 5)
+                    let nad = SCNNode.init(geometry: nadGeo)
+                    nad.geometry?.firstMaterial?.diffuse.contents = NSColor.darkGray
+                    nad.position = (protein?.position)!
+                    nad.name = "NAD-\(i)"
+                    let movement = SCNAction.move(by: self.subtract(vector1: (protein?.position)!,
+                                                                    vector2: SCNVector3Make(CGFloat(self.randomIntFrom(start: -30, to: 80)),
+                                                                                            CGFloat(self.randomIntFrom(start: 10, to: 50)),
+                                                                                            CGFloat(self.randomIntFrom(start: -50, to: 130)))), duration: 2)
+                    nad.runAction(SCNAction.sequence([movement, randomMovement]))
+                    
+                    self.sceneView.scene?.rootNode.addChildNode(nad)
+                })
                 self.sceneView.scene?.rootNode.addChildNode(h)
             })
         }
@@ -53,6 +82,16 @@ class GameViewController: NSViewController {
         nad.runAction(molecularAnimation())
         nad.name = name
         return nad
+    }
+    
+    func createH(position: SCNVector3, name: String) -> SCNNode {
+        
+        let hGeo = SCNSphere.init(radius: 2)
+        let h = SCNNode.init(geometry: hGeo)
+        h.geometry?.firstMaterial?.diffuse.contents = NSColor.blue
+        h.position = position
+        h.runAction(molecularAnimation())
+        return h
     }
     
     func molecularAnimation() -> SCNAction {
@@ -130,13 +169,22 @@ class GameViewController: NSViewController {
         // create a new scene
         let scene = SCNScene(named: "tinker.scn")!
         
-        buildMembrane(layers: 1, width: 1, rootNode: scene.rootNode)
+        buildMembrane(layers: 1, width: 2, rootNode: scene.rootNode)
         buildProteins(rootNode: scene.rootNode)
         for i in 0...10 {
-            scene.rootNode.addChildNode(createNADH(position: SCNVector3Make(CGFloat(randomIntFrom(start: -50, to: 60)),
+            scene.rootNode.addChildNode(createNADH(position: SCNVector3Make(CGFloat(randomIntFrom(start: -30, to: 80)),
                                                                             CGFloat(randomIntFrom(start: -80, to: -130)),
-                                                                            CGFloat(randomIntFrom(start: -50, to: 100))), name: "nadh-\(i)"))
+                                                                            CGFloat(randomIntFrom(start: -50, to: 130))), name: "nadh-\(i)"))
         }
+        
+        for i in 0...20 {
+            scene.rootNode.addChildNode(createH(position: SCNVector3Make(CGFloat(randomIntFrom(start: -10, to: 80)),
+                                                                         CGFloat(randomIntFrom(start: +40, to: +80)),
+                                                                         CGFloat(randomIntFrom(start: -50, to: 170))), name: "nadh-\(i)"))
+        }
+        
+        let aptProtein = scene.rootNode.childNode(withName: "atp-protein", recursively: true)
+        aptProtein?.scale = SCNVector3Make(0.7, 0.7, 0.7)
         
         // set the scene to the view
         self.sceneView.scene = scene
